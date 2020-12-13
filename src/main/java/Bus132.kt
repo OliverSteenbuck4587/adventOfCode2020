f8b63d39892342f4922bb655c6f1646de9968a57
os <ft6@otto.de>
2020-12-13 18:20:39 +0100

    task 📝|os|document the nicer way to do it :D
    Co-authored-by: Oliver Steenbuck <oliver.steenbuck@otto.de>

--- src/main/java/Bus132.kt
+++ src/main/java/Bus132.kt
@@ -1,42 +1,44 @@
-
-
-
 class Bus132 {
 
     fun run() {
         val input = readFileAsLinesUsingGetResourceAsStream("13bus.txt")
         val buses = input.split("\n")[1]
                 .split(",")
-                .mapIndexed{ index,it -> Pair(index,it.trim()) }
+                .mapIndexed { index, it -> Pair(index, it.trim()) }
                 .filter { it.second != "x" }
-                .map{Bus(it.first,it.second.toInt() )}
+                .map { Bus(it.first, it.second.toInt()) }
 
-//        var i = 0;
-//        val increment = buses.maxByOrNull { it.id }!!
-//        while(!isSolution(buses,i)){
-//            if(i % 100000 == 0)
-//                println("Candidate: " + i)
-//            i++
+// a nicer way to go about this would be to get the increment by combining busses
+// shamelessy copied from https://todd.ginsberg.com/post/advent-of-code/2020/day13/
+//fun solvePart2(): Long {
+//    var stepSize = indexedBusses.first().bus
+//    var time = 0L
+//    indexedBusses.drop(1).forEach { (offset, bus) ->
+//        while ((time + offset) % bus != 0L) {
+//            time += stepSize
 //        }
-
+//        stepSize *= bus // New Ratio!
+//    }
+//    return time
+//}
         var i = 0.toLong();
         val increment = buses.maxByOrNull { it.departureTime }!!
 
         do {
-            if(i % 1000000.toLong() == 0.toLong())
+            if (i % 1000000.toLong() == 0.toLong())
                 println("Candidate: " + i)
-            i+=increment.departureTime
-        }while(!isSolution(buses,i-increment.id))
+            i += increment.departureTime
+        } while (!isSolution(buses, i - increment.id))
 
 
 
-        println("Solution: " + (i-increment.id))
+        println("Solution: " + (i - increment.id))
 
 
     }
 
-    fun isSolution(buses: List<Bus>, start: Long): Boolean{
-        return buses.all { (start+it.id)%it.departureTime == 0.toLong()}
+    fun isSolution(buses: List<Bus>, start: Long): Boolean {
+        return buses.all { (start + it.id) % it.departureTime == 0.toLong() }
     }
 
     fun readFileAsLinesUsingGetResourceAsStream(fileName: String) = this::class.java.getResourceAsStream(fileName).bufferedReader().readText()
